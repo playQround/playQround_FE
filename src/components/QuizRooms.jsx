ea73f6fe75027ad1260c017d106a4160ac7350b5
Chunws13 <dntkd51@naver.com>
2023-07-08 19:50:33 +0900

    방 상태 변경

--- src/components/QuizRooms.jsx
+++ src/components/QuizRooms.jsx
@@ -1,10 +1,33 @@
 import "../css/QuizRoom.css";
 
+// 방 상태에 따라서 이름 분류
+const RoomStatusCheck = ({ status }) => {
+    let className ;
+    let roomStatusToString;
+
+    if (status === 0) {
+        roomStatusToString = "Waiting";
+        className = "Waiting"
+
+    } else if (status === 1){
+        roomStatusToString = "Playing";
+        className = "Playing"
+
+    } else {
+        roomStatusToString = "Done";
+        className = "Done"
+    }
+        
+    return <p className={className}> {roomStatusToString} </p>
+};
+
+
 const EachRoom = ({ socketRoom, name, status, now, max, setSelectedRoom }) => {
+    
     return (
         <div className="QuizRoom" onClick={() => setSelectedRoom(socketRoom)}>
             <p className="RoomName"> {name} </p>
-            <p className="RoomStatus"> {status} </p>
+            <RoomStatusCheck status={status} />
             <p className="RoomPeople">
                 {" "}
                 {now} / {max}{" "}
@@ -15,24 +38,21 @@ const EachRoom = ({ socketRoom, name, status, now, max, setSelectedRoom }) => {
 
 const QuizRooms = ({ quizRoom, setSelectedRoom }) => {
     return (
-        <>
-            <div className="QuizzesRoomArea">
-                {quizRoom.map((item, index) => {
-                    return (
-                        <EachRoom
-                            key={index}
-                            socketRoom={item._id}
-                            name={item.roomName}
-                            status={item.roomStatus}
-                            now={item.nowPeople}
-                            max={item.maxPeople}
-                            setSelectedRoom={setSelectedRoom}
-                        />
-                    );
-                })}
-            </div>
-        </>
-    );
+        <div className="QuizzesRoomArea">
+            {quizRoom.map((item, index) => {
+                return (
+                    <EachRoom
+                        key={index}
+                        socketRoom={item._id}
+                        name={item.roomName}
+                        status={item.roomStatus}
+                        now={item.nowPeople}
+                        max={item.maxPeople}
+                        setSelectedRoom={setSelectedRoom}
+                    />);
+            })}
+        </div>
+    )
 };
 
 export default QuizRooms;
