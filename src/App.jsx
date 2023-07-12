36abf56131c19980e6133ad85997fd9e8e2ae0c7
Chunws13 <dntkd51@naver.com>
2023-07-13 02:18:01 +0900

    rtc 적용

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     useEffect(() => {
         const socketIo = io(process.env.REACT_APP_SERVER_URL);
         setSocket(socketIo);        
-        const webRtcSocketIo = io("http://localhost:3000");
+        const webRtcSocketIo = io("https://socket.playqround.site/");
         setWebRtcSocket(webRtcSocketIo);
         return () => {
             if (socket) {
